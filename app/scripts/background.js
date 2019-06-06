35707c6cec0ae4aff4689480baabab3097463764
ravemir <ravemjr@gmail.com>
2019-06-06 21:03:47 +0100

    First codebase commit in a while.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,7 +1,5 @@
-'use strict';
+'use strict'
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+chrome.runtime.onInstalled.addListener(details => {
+	console.log('previousVersion', details.previousVersion);
 });
-
-console.log('\'Allo \'Allo! Event Page');
